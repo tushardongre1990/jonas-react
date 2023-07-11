a1e3312a9be753333bf93301bb05a7ff23bd135b
Tushar Dongre <tushardongre.1990@gmail.com>
2023-07-11 12:22:31 +0530

    react fragment

--- src/App.js
+++ src/App.js
@@ -68,13 +68,20 @@ const Menu = () => {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+
       {/* renders list of pizza only if there is data in pizzaData */}
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine, 6 creative dishes to choose from. All
+            from our stone oven. all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later</p>
       )}
