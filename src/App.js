ab10e90d55429141f5e4d7414ce91681f7b70ebd
Tushar Dongre <tushardongre.1990@gmail.com>
2023-07-13 19:06:50 +0530

    event handlers

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ const messages = [
 
 function App() {
   const step = 1;
+  function handlePrevious() {
+    alert("Previous");
+  }
+  function handleNext() {
+    alert("Next");
+  }
   return (
     <div className="steps">
       <div className="numbers">
@@ -17,10 +23,16 @@ function App() {
         Step {step} : {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
