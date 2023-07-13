092578cc4e7ad6c8638eda92afe1b04b34318cd8
Tushar Dongre <tushardongre.1990@gmail.com>
2023-07-13 20:49:34 +0530

    65 state update based on current state

--- src/App.js
+++ src/App.js
@@ -12,10 +12,13 @@ function App() {
 
   const [isOpen, setIsOpen] = useState(true);
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) {
+      setStep((s) => s + 1);
+      // setStep((s) => s + 1);
+    }
     // Bad Practice
     // test.name = "Jason";
     // setTest({ name: "Jason" });
@@ -25,7 +28,7 @@ function App() {
       <button
         className="close"
         onClick={() => {
-          setIsOpen(!isOpen);
+          setIsOpen((is) => !is);
         }}
       >
         &times;
