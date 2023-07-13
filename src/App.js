01432f77475abddb8785eda2a9728255594ae406
Tushar Dongre <tushardongre.1990@gmail.com>
2023-07-13 18:43:30 +0530

    steps components

--- src/App.js
+++ src/App.js
@@ -1,144 +1,30 @@
-import "./index.css";
-const pizzaData = [
-  {
-    name: "Focaccia",
-    ingredients: "Bread with italian olive oil and rosemary",
-    price: 6,
-    photoName: "pizzas/focaccia.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Margherita",
-    ingredients: "Tomato and mozarella",
-    price: 10,
-    photoName: "pizzas/margherita.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Spinaci",
-    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-    price: 12,
-    photoName: "pizzas/spinaci.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Funghi",
-    ingredients: "Tomato, mozarella, mushrooms, and onion",
-    price: 12,
-    photoName: "pizzas/funghi.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Salamino",
-    ingredients: "Tomato, mozarella, and pepperoni",
-    price: 15,
-    photoName: "pizzas/salamino.jpg",
-    soldOut: true,
-  },
-  {
-    name: "Pizza Prosciutto",
-    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-    price: 18,
-    photoName: "pizzas/prosciutto.jpg",
-    soldOut: false,
-  },
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
 ];
 
 function App() {
+  const step = 1;
   return (
-    <div className="container">
-      <Header />
-      <Menu />
-      <Footer />
-    </div>
-  );
-}
-
-const Header = () => {
-  const style = {};
-  return (
-    <header className="header">
-      <h1 style={style}>Fast React Pizza Co.</h1>
-    </header>
-  );
-};
-
-const Menu = () => {
-  const pizzas = pizzaData;
-  const numPizzas = pizzas.length;
-  return (
-    <main className="menu">
-      <h2>Our Menu</h2>
-
-      {/* renders list of pizza only if there is data in pizzaData */}
-      {numPizzas > 0 ? (
-        <>
-          <p>
-            Authentic Italian cuisine, 6 creative dishes to choose from. All
-            from our stone oven. all organic, all delicious.
-          </p>
-          <ul className="pizzas">
-            {pizzaData.map((pizza) => (
-              <Pizza pizzaObj={pizza} key={pizza.name} />
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>We're still working on our menu. Please come back later</p>
-      )}
-    </main>
-  );
-};
-
-function Pizza({ pizzaObj }) {
-  // if (pizzaObj.soldOut) return null;
-  return (
-    <div className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
-      <img src={pizzaObj.photoName} alt={pizzaObj.name} />
-      <li>
-        <h3>{pizzaObj.name}</h3>
-        <p>{pizzaObj.ingredients}</p>
-        {/* {pizzaObj.soldOut ? (
-          <span>SOLD OUT</span>
-        ) : (
-          <span>{pizzaObj.price}</span>
-        )} */}
-        <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
-      </li>
-    </div>
-  );
-}
-
-const Footer = () => {
-  const hour = new Date().getHours();
-
-  const openHour = 10;
-  const closeHour = 22;
-  const isOpen = hour >= openHour && hour <= closeHour;
-
-  return (
-    <footer className="footer">
-      {isOpen ? (
-        <Order openHour={openHour} closeHour={closeHour} />
-      ) : (
-        <p>
-          We're happy to welcome you between {openHour}:00 and {closeHour}:00
-        </p>
-      )}
-    </footer>
-  );
-};
-
-function Order({ openHour, closeHour }) {
-  return (
-    <div className="order">
-      <p>
-        We are open from {openHour}:00 until {closeHour}:00. Come visit us or
-        order online
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step} : {messages[step - 1]}
       </p>
-      <button className="btn">Order</button>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
     </div>
   );
 }
-
 export default App;
