127bc297da9eae8bda7f39e53777c933ffeb8326
Tushar Dongre <tushardongre.1990@gmail.com>
2023-08-20 00:51:59 +0530

    73.Controlled Elements

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Chaddi", quantity: 4, packed: true },
@@ -18,15 +20,31 @@ export default function App() {
 function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
+
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(event);
+    // console.log(event);
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    // sets the quantity and description back to initial state after form submission
+    setDescription("");
+    setQuantity(1);
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(event) => setQuantity(Number(event.target.value))}
+      >
         {Array.from({ length: 20 }, (currentValue, index) => index + 1).map(
           (num) => (
             <option value={num} key={num}>
@@ -35,11 +53,18 @@ function Form() {
           )
         )}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
       <button>Add</button>
     </form>
   );
 }
+
+// Form and PackingList are sibling components so we have pass data from Form to PackingList directly via props
 function PackingList() {
   return (
     <div className="list">
