b06349f397dd5a2c172916c7e1a0182906861db8
Tushar Dongre <tushardongre.1990@gmail.com>
2023-08-19 23:42:57 +0530

    72 Building Form and Handling Submission

--- src/App.js
+++ src/App.js
@@ -19,10 +19,25 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 function Form() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (currentValue, index) => index + 1).map(
+          (num) => (
+            <option value={num} key={num}>
+              {num}
+            </option>
+          )
+        )}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
@@ -30,7 +45,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
