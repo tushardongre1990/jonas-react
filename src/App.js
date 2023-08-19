3e5ebba1846ddaecb5e5c7ef1bdb42a34ddb3c8d
Tushar Dongre <tushardongre.1990@gmail.com>
2023-08-19 23:17:53 +0530

    71.Rendering List Items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Chaddi", quantity: 4, packed: true },
+  { id: 3, description: "Socks", quantity: 12, packed: false },
 ];
 
 export default function App() {
@@ -24,29 +25,29 @@ function Form() {
     </div>
   );
 }
-// function PackingList() {
-//   return (
-//     <div className="list">
-//       <ul>
-//         {initialItems.map((item) => (
-//           <Item item={item} />
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
-
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-// function Item({ item }) {
-//   return (
-//     <>
-//       <li>{item.description}</li>
-//     </>
-//   );
-// }
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
