5444f8c0e1b4e67dbe6292106ee473260e519d72
Tushar Dongre <tushardongre.1990@gmail.com>
2023-08-21 17:38:22 +0530

    80.Thinking About State and Lifting State Up

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]); // lifted up
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,9 +26,18 @@ function Logo() {
   return <h1>🌴 Far Away 💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
+  // const [items, setItems] = useState([]); // empty array → since the displayed items are packing item which are array
+
+  // function handleAddItems(item) {
+  //   setItems((item) => [...items, item]);
+  // }  // move to App
+
+  // right now our item state is inside form component, but we need this state inside PackingList component 'cause in PackingList it should be rendered but we can't pass it as a prop since PackingList is sibling of Form
+  // To solve this, we lift the state i.e const [items, setItems] = useState([]); to the closest parent component i.e App component
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -33,6 +47,8 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     // sets the quantity and description back to initial state after form submission
     setDescription("");
@@ -65,11 +81,11 @@ function Form() {
 }
 
 // Form and PackingList are sibling components so we have pass data from Form to PackingList directly via props
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
