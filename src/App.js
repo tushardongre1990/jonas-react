f067f0ab38a21e157b3e3ea9127e2d4de7949582
Tushar Dongre <tushardongre.1990@gmail.com>
2023-08-22 19:11:03 +0530

    82.Deleting Items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Chaddi", quantity: 4, packed: true },
-  { id: 3, description: "Socks", quantity: 12, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Chaddi", quantity: 4, packed: true },
+//   { id: 3, description: "Socks", quantity: 12, packed: false },
+// ];
 
 export default function App() {
   const [items, setItems] = useState([]); // lifted up
@@ -12,11 +12,17 @@ export default function App() {
   function handleAddItems(item) {
     setItems((items) => [...items, item]);
   }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  // now pass handleAddItems to PackingList as a prop
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -81,25 +87,28 @@ function Form({ onAddItems }) {
 }
 
 // Form and PackingList are sibling components so we have pass data from Form to PackingList directly via props
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      {/* button click will happen in Item comp but the state lives in App Comp (parent comp) child to parent communication */}
+      {/* <button onClick={onDeleteItem}>❌</button> */}
+      {/* this won't work because when we simply specify the function, react calls the function when event happens by passing in the event object, but we don't want event we want id of current object therefore below button code*/}
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
