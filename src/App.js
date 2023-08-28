a9c43cb333e23c2796626156dfc3b181946d26aa
Tushar Dongre <tushardongre.1990@gmail.com>
2023-08-28 21:03:40 +0530

    96 Static App : List of Friends

--- src/App.js
+++ src/App.js
@@ -1,190 +1,65 @@
-import { useState } from "react";
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Chaddi", quantity: 4, packed: true },
-//   { id: 3, description: "Socks", quantity: 12, packed: false },
-// ];
-
-export default function App() {
-  const [items, setItems] = useState([]); // lifted up
-
-  function handleAddItems(item) {
-    setItems((items) => [...items, item]);
-  }
-
-  function handleDeleteItem(id) {
-    setItems((items) => items.filter((item) => item.id !== id));
-  }
-  // now pass handleAddItems to PackingList as a prop
-
-  function handleToggleItem(id) {
-    setItems((items) =>
-      items.map((item) =>
-        item.id === id ? { ...item, packed: !item.packed } : item
-      )
-    );
-  }
-
-  function handleClearList() {
-    const confirmed = window.confirm(
-      "Are you sure you want to delete all the items"
-    );
-    if (confirmed) setItems([]);
-  }
-
+function App() {
   return (
     <div className="app">
-      <Logo />
-      <Form onAddItems={handleAddItems} />
-      <PackingList
-        items={items}
-        onDeleteItem={handleDeleteItem}
-        onToggleItem={handleToggleItem}
-        onClearList={handleClearList}
-      />
-      <Stats items={items} />
-    </div>
-  );
-}
-
-function Logo() {
-  return <h1>🌴 Far Away 💼</h1>;
-}
-
-function Form({ onAddItems }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  // const [items, setItems] = useState([]); // empty array → since the displayed items are packing item which are array
-
-  // function handleAddItems(item) {
-  //   setItems((item) => [...items, item]);
-  // }  // move to App
-
-  // right now our item state is inside form component, but we need this state inside PackingList component 'cause in PackingList it should be rendered but we can't pass it as a prop since PackingList is sibling of Form
-  // To solve this, we lift the state i.e const [items, setItems] = useState([]); to the closest parent component i.e App component
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    // console.log(event);
-
-    if (!description) return;
-
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
-
-    onAddItems(newItem);
-
-    // sets the quantity and description back to initial state after form submission
-    setDescription("");
-    setQuantity(1);
-  }
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip</h3>
-      <select
-        value={quantity}
-        onChange={(event) => setQuantity(Number(event.target.value))}
-      >
-        {Array.from({ length: 20 }, (currentValue, index) => index + 1).map(
-          (num) => (
-            <option value={num} key={num}>
-              {num}
-            </option>
-          )
-        )}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(event) => setDescription(event.target.value)}
-      />
-      <button>Add</button>
-    </form>
-  );
-}
-
-// Form and PackingList are sibling components so we have pass data from Form to PackingList directly via props
-function PackingList({ items, onDeleteItem, onToggleItem, onClearList }) {
-  const [sortBy, setSortBy] = useState("input");
-
-  let sortedItems;
-
-  if (sortBy === "input") sortedItems = items;
-  if (sortBy === "description")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-
-  if (sortBy === "packed")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
-  return (
-    <div className="list">
-      <ul>
-        {/* {items.map((item) => ( */}{" "}
-        {/* we need to display the sorted elements so replace items with sortedItems */}
-        {sortedItems.map((item) => (
-          <Item
-            item={item}
-            onDeleteItem={onDeleteItem}
-            onToggleItem={onToggleItem}
-            key={item.id}
-          />
-        ))}
-      </ul>
-      <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Sort by input order</option>
-          <option value="description">Sort by description</option>
-          <option value="packed">Sort by packed status</option>
-        </select>
-        <button onClick={onClearList}>Clear List</button>
+      <div className="sidebar">
+        <FriendList />
       </div>
     </div>
   );
 }
 
-function Item({ item, onDeleteItem, onToggleItem }) {
+function FriendList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
   return (
     <li>
-      <input
-        type="checkbox"
-        value={item.packed}
-        onChange={() => onToggleItem(item.id)}
-      />
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description}
-      </span>
-      {/* button click will happen in Item comp but the state lives in App Comp (parent comp) child to parent communication */}
-      {/* <button onClick={onDeleteItem}>❌</button> */}
-      {/* this won't work because when we simply specify the function, react calls the function when event happens by passing in the event object, but we don't want event we want id of current object therefore below button code*/}
-      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+      <img src={friend.image} alt={friend.name} />
+      <h3> {friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owe you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You owe {friend.name} are even</p>}
+      <button className="button">Select</button>
     </li>
   );
 }
 
-function Stats({ items }) {
-  if (!items.length)
-    return (
-      <p className="stats">
-        {" "}
-        <em>Start adding some item to your packing list 🚀</em>
-      </p>
-    );
-  const numItems = items.length;
-  const numPacked = items.filter((item) => item.packed).length;
-  const percentage = Math.round((numPacked * 100) / numItems);
-  return (
-    <footer className="stats">
-      <em>
-        {percentage === 100
-          ? "You got everything! Ready to go ✈"
-          : `💼 Yo have ${numItems} items on your list, and you have already packed ${numPacked} ( ${percentage} ) %`}
-      </em>
-    </footer>
-  );
-}
+export default App;
