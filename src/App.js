b8a4ef5287645dc6e774dc9613bb5e78ed00c30e
Tushar Dongre <tushardongre.1990@gmail.com>
2023-08-29 00:48:07 +0530

    97 Static Form

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ function App() {
     <div className="app">
       <div className="sidebar">
         <FriendList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -57,8 +60,50 @@ function Friend({ friend }) {
         </p>
       )}
       {friend.balance === 0 && <p>You owe {friend.name} are even</p>}
-      <button className="button">Select</button>
+
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label htmlFor="">👨‍👧Friend Name</label>
+      <input type="text" />
+      <label htmlFor="">🖼 Image Url</label>
+      <input type="text" />
+      <Button>Add friend</Button>
+    </form>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label htmlFor="">💰 Bill Value</label>
+      <input type="text" />
+
+      <label htmlFor="">👨 Your Expense</label>
+      <input type="text" />
+
+      <label htmlFor="">👨‍👧 X's Expense</label>
+      <input type="text" disabled />
+
+      <label htmlFor="">🤑 Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Add friend</Button>
+    </form>
   );
 }
 
