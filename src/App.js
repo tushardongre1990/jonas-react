7d0ed759d1982ee22e37c3113850478efc3c6b8e
tushardongre1990 <tushardongre.1990@gmail.com>
2023-09-21 18:58:42 +0530

    108.Splitting Code

--- src/App.js
+++ src/App.js
@@ -1,114 +1,239 @@
-const pizzaData = [
+import { useState } from "react";
+
+const tempMovieData = [
   {
-    name: "Focaccia",
-    ingredients: "Bread with italian olive oil and rosemary",
-    price: 6,
-    photoName: "pizzas/focaccia.jpg",
-    soldOut: false,
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
   },
   {
-    name: "Pizza Margherita",
-    ingredients: "Tomato and mozarella",
-    price: 10,
-    photoName: "pizzas/margherita.jpg",
-    soldOut: false,
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
   },
   {
-    name: "Pizza Spinaci",
-    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-    price: 12,
-    photoName: "pizzas/spinaci.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Funghi",
-    ingredients: "Tomato, mozarella, mushrooms, and onion",
-    price: 12,
-    photoName: "pizzas/funghi.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Salamino",
-    ingredients: "Tomato, mozarella, and pepperoni",
-    price: 15,
-    photoName: "pizzas/salamino.jpg",
-    soldOut: true,
-  },
-  {
-    name: "Pizza Prosciutto",
-    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-    price: 18,
-    photoName: "pizzas/prosciutto.jpg",
-    soldOut: false,
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
   },
 ];
 
-function App() {
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+
+function Search() {
+  const [query, setQuery] = useState("");
   return (
-    <div className="container">
-      <Header />
-      <Menu />
-      <Footer />
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+
+function Logo(){
+  return(
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
     </div>
   );
 }
 
-const Header = () => {
-  // const style = { color: "red", fontSize: "40px", textTransform: "uppercase" };
-  const style = {};
-  return (
-    <header className="header">
-      <h1 style={style}>Fast React Pizza Co.</h1>
-    </header>
-  );
-};
+  function NavBar(){
+    
+    return <nav className="nav-bar">
+    <Logo/>
+    <Search/>
+    <NumResults/>
+    
+    
+  </nav>
 
-const Menu = () => {
-  return (
-    <main className="menu">
-      <h2>Our Menu</h2>
-      <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={10}
-      />
-      <Pizza
-        name="Pizza Funghi"
-        ingredients="Tomato, mozarella, mushrooms, and onion"
-        photoName="pizzas/funghi.jpg"
-        price={15}
-      />
-    </main>
-  );
-};
+  }
 
-function Pizza(props) {
-  return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+  function NumResults(){
+    return (<p className="num-results">
+    Found <strong>X</strong> results
+  </p>);
+  }
+
+  function Main(){
+
+    
+   
+    
+    
+    return(
+    <main className="main">
+
+      <ListBox/>
+      <WatchedBox/>
+    
+
+    
+  </main>);
+  }
+
+  function ListBox(){
+    const [isOpen1, setIsOpen1] = useState(true);
+    
+    return (
+      <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen1((open) => !open)}
+      >
+        {isOpen1 ? "–" : "+"}
+      </button>
+      {isOpen1 && (
+        <MovieList/>
+      )}
+    </div>
+    );
+  }
+  function MovieList(){
+    const [movies, setMovies] = useState(tempMovieData);
+    return(<ul className="list">
+    {movies?.map((movie) => (
+      <Movie movie={movie} key={movie.imdbID}/>
+    ))}
+  </ul>);
+  }
+
+  function Movie({movie}){
+    return (<li >
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
       <div>
-        <h2>{props.name}</h2>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
       </div>
+    </li>);
+  }
+
+  function WatchedBox(){
+
+    const [isOpen2, setIsOpen2] = useState(true);
+    const [watched, setWatched] = useState(tempWatchedData);
+    
+    return(
+    <div className="box">
+      <button
+        className="btn-toggle"
+        onClick={() => setIsOpen2((open) => !open)}
+      >
+        {isOpen2 ? "–" : "+"}
+      </button>
+      {isOpen2 && (
+        <>
+         <WatchedSummary watched={watched}/>
+
+        <WatchedMovieList watched={watched}/>
+        </>
+      )}
+    </div>);
+  }
+  
+  
+  export default function App(){
+    return(
+      <>
+      <NavBar/>
+      <Main/>
+      </>
+    );
+  }
+
+  function WatchedSummary({watched}){
+    const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+    const avgUserRating = average(watched.map((movie) => movie.userRating));
+    const avgRuntime = average(watched.map((movie) => movie.runtime));
+    return( <div className="summary">
+    <h2>Movies you watched</h2>
+    <div>
+      <p>
+        <span>#️⃣</span>
+        <span>{watched.length} movies</span>
+      </p>
+      <p>
+        <span>⭐️</span>
+        <span>{avgImdbRating}</span>
+      </p>
+      <p>
+        <span>🌟</span>
+        <span>{avgUserRating}</span>
+      </p>
+      <p>
+        <span>⏳</span>
+        <span>{avgRuntime} min</span>
+      </p>
     </div>
-  );
-}
+  </div>);
+  }
 
-const Footer = () => {
-  const hour = new Date().getHours();
+  function WatchedMovieList({watched}){
+    return (
+      <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie = {movie} key={movie.imdbID} />
+      ))}
+    </ul>
+    );
+  }
 
-  const openHour = 12;
-  const closeHour = 22;
-  const isOpen = hour >= openHour && hour <= closeHour;
-
-  const time = new Date().toLocaleTimeString();
-  return (
-    <footer className="footer">
-      <p>{time} We are currently open</p>
-    </footer>
-  );
-};
-
-export default App;
+  function WatchedMovie({movie}){
+    return (<li >
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+    </li>);
+  }
